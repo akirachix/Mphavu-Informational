2ac82666c1b60d6cd91589ab6c5f2e9b169b50d9
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-09-21 20:39:29 +0300

    updated workflow

--- informational-website/src/app/page.tsx
+++ informational-website/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import FeaturedAcademies from "./components/FeaturedAcademies/page";
 import Team from "./components/Team/page";
 import HeroSection from "./components/HeroSection";
