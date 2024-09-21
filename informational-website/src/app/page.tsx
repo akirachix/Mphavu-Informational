24931b09ecdf020fa2a1e58bfde49c5f9c919aed
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-09-21 17:38:25 +0300

    created teams

--- informational-website/src/app/page.tsx
+++ informational-website/src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
 import FeaturedAcademies from "./components/FeaturedAcademies/page";
 import Team from "./components/Team/page";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <main>
       <div>
+        <Navbar/>
+        <HeroSection/>
         <FeaturedAcademies/>
         <Team/>
+
       </div>
     </main>
   );
